ca778bc068dbcef0f374fbc72023edefb3172d0e
wsj3 <johnstonswill@gmail.com>
2025-03-02 13:23:17 -0800

    Implement Vercel support recommendations: Downgrade to Next.js 14, remove app dir, reset dependencies, add cache key

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import '../src/app/globals.css';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,7 @@ module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/**/*.{js,ts,jsx,tsx,mdx}',
+    './styles/**/*.{css,scss}',
   ],
   theme: {
     extend: {},

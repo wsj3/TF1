70317852cab4fc5f521fac26842a08c5c76b4924
wsj3 <johnstonswill@gmail.com>
2025-03-04 09:08:45 -0800

    Update SessionProvider configuration for better reliability

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,11 @@ import { SessionProvider } from 'next-auth/react';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
+  // Use the layout defined at the page level, if available
   return (
     <SessionProvider 
       session={pageProps.session}
-      // Re-fetch session every 5 minutes
+      // Force refresh the session every 5 minutes
       refetchInterval={5 * 60}
       // Re-fetch session when window focuses
       refetchOnWindowFocus={true}
